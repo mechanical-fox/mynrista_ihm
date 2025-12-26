c79ad08087c9174356a0d895d782f4f36a8d2360
Pierre Meunier <meunier.pierre108@gmail.com>
2025-12-26 11:59:45 +0100

    :sparkles: Amélioration template Vue

--- src/component/App.vue
+++ src/component/App.vue
@@ -2,8 +2,17 @@
 
 
 <template>
-	<link rel="stylesheet" href="css/style.css" />
-    <p class="text"> {{message}}</p>
+    
+
+	<div class="div-1">
+		<p class="text"> Le compteur vaut {{counter}}</p>
+		<br/>
+		<p class="text"> Compteur Supérieur à 5:</p>
+		<p class="text" v-if="mostThan5"> &nbsp;&nbsp;&nbsp;&nbsp; Oui</p>
+		<p class="text" v-else> &nbsp;&nbsp;&nbsp;&nbsp; Non</p>
+		<br/>
+		<button class="button-1" @click="increment()"> Clic </button>
+	</div>
 
 </template>
 
@@ -12,7 +21,15 @@
 
 	import {ref} from 'vue';
 
-	const message = ref("Hello world");
+	const mostThan5 = ref(false);
+	const counter = ref(0);
+
+	function increment(){
+		counter.value++;
+
+		if(counter.value > 5)
+			mostThan5.value = true;
+	}
 
 </script>
 
